af47274f4211802f845c4b4f7f4bad9ead2f9992
whohlfeld <33004991+whohlfeld@users.noreply.github.com>
2017-12-13 14:35:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-__author__ = 'mmeyer'
+__author__ = 'whohlfeld'
 
 LOGGER = getLogger(__name__)
 
@@ -22,45 +22,26 @@ DRADIO_URL = 'http://st02.dlf.de/dlf/02/128/mp3/stream.mp3'
 NOVA_URL = 'http://st03.dlf.de/dlf/03/128/mp3/stream.mp3'
 
 
-class DlfSkill(MycroftSkill):
+class RadioSkill(MycroftSkill):
     def __init__(self):
-        super(DlfSkill, self).__init__(name="DlfSkill")
+        super(RadioSkill, self).__init__(name="RadioSkill")
         self.audioservice = None
 
     def initialize(self):
         if AudioService:
             self.audioservice = AudioService(self.emitter)
 
-        whatson_dlf_intent = IntentBuilder("WhatsonDlfIntent").\
+        whatson_radio_intent = IntentBuilder("WhatsonRadioIntent").\
                          require("WhatsonKeyword").\
-                         require("DlfKeyword").build()
-        self.register_intent(whatson_dlf_intent, self.handle_whatson_dlf_intent)
+                         require("RadioKeyword").build()
+        self.register_intent(whatson_dlf_intent, self.handle_whatson_radio_intent)
 
-        whatson_dradio_intent = IntentBuilder("WhatsonDradioIntent").\
-                                require("WhatsonKeyword").\
-                                require("DradioKeyword").build()
-        self.register_intent(whatson_dradio_intent,
-                             self.handle_whatson_dradio_intent)
+        radio_intent = IntentBuilder("RadioIntent").\
+                        require("RadioKeyword").\
+                        require("PlayKeyword").build()
+        self.register_intent(dlf_intent, self.handle_radio_intent)
 
-        whatson_nova_intent = IntentBuilder("WhatsonNovaIntent").\
-                              require("WhatsonKeyword").\
-                              require("NovaKeyword").build()
-        self.register_intent(whatson_nova_intent,
-                             self.handle_whatson_nova_intent)
-
-        dlf_intent = IntentBuilder("DlfIntent").\
-                     require("DlfKeyword").require("PlayKeyword").build()
-        self.register_intent(dlf_intent, self.handle_dlf_intent)
-
-        dradio_intent = IntentBuilder("DradioIntent").\
-                        require("DradioKeyword").require("PlayKeyword").build()
-        self.register_intent(dradio_intent, self.handle_dradio_intent)
-
-        nova_intent = IntentBuilder("NovaIntent").\
-                      require("NovaKeyword").require("PlayKeyword").build()
-        self.register_intent(nova_intent, self.handle_nova_intent)
-
-    def handle_whatson_dlf_intent(self, message):
+    def handle_whatson_radio_intent(self, message):
         r = requests.get('http://www.deutschlandfunk.de')
         soup = BeautifulSoup(r.text)
         for el in soup.find_all(id='dlf-player-jetzt-im-radio'):
@@ -68,42 +49,15 @@ class DlfSkill(MycroftSkill):
                 self.speak_dialog("currently",
                                   { "station": "dlf", "title": a_el.string})
 
-    def handle_whatson_dradio_intent(self, message):
-        r = requests.get('http://www.deutschlandfunkkultur.de/')
-        soup = BeautifulSoup(r.text)
-        for el in soup.find_all(id='drk-player-jetzt-im-radio'):
-            for a_el in el.find_all('a'):
-                self.speak_dialog("currently",
-                                  { "station": "dlf culture", "title": a_el.string})
-
-    def handle_whatson_nova_intent(self, message):
-        r = requests.get('https://www.deutschlandfunknova.de/actions/dradio/playlist/onair')
-        j = r.json()
-        
-        self.speak_dialog("currently",
-                          {"station": "dlf nova", "title": j['show']['title']})
-
-    def handle_dlf_intent(self, message):
+    def handle_radio_intent(self, message):
         if self.audioservice:
             self.audioservice.play(DLF_URL, message.data['utterance'])
         else:
             self.process = play_mp3(DLF_URL)
-
-    def handle_dradio_intent(self, message):
-        if self.audioservice:
-            self.audioservice.play(DRADIO_URL, message.data['utterance'])
-        else:
-            self.process = play_mp3(DRADIO_URL)
-
-    def handle_nova_intent(self, message):
-        if self.audioservice:
-            self.audioservice.play(NOVA_URL, message.data['utterance'])
-        else:
-            self.process = play_mp3(NOVA_URL)
 
     def stop(self):
         pass
 
 
 def create_skill():
-    return DlfSkill()
+    return RadioSkill()
